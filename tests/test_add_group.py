0ba1c80e30fb8fd5235fe3b55745cc1b8d5d7159
Semonok <semonok95@gmail.com>
2018-03-11 03:45:01 +0500

    modified methods for optimizing AT scripts

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,7 +3,5 @@ from model.group import Group
 
 def test_add_group(app):
     app.session.Login(username="admin", password="secret")
-    app.group_managment.open_group()
     app.group_managment.Create_new_group(Group(name="123", header="321", footer="195"))
-    app.group_managment.return_to_group()
     app.session.Logout()
--- tests/test_del_group.py
+++ tests/test_del_group.py
@@ -1,9 +1,7 @@
 
 def test_add_group(app):
     app.session.Login(username="admin", password="secret")
-    app.group_managment.open_group()
     app.group_managment.delete_group()
-    app.group_managment.return_to_group()
     app.session.Logout()
 
 
