566147b83fb1a5d677960b912ecc2d5ed71f3ccc
Semonok <semonok95@gmail.com>
2018-03-11 16:09:35 +0500

    Upgrade fixture and test_modify_group

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -2,6 +2,5 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.Login(username="admin", password="secret")
     app.group_managment.Create_new_group(Group(name="123", header="321", footer="195"))
-    app.session.Logout()
+
--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 
-def test_modify_group(app):
-    app.session.Login(username="admin", password="secret")
+def test_modify_group_name(app):
+    app.group_managment.modify_first_group(Group(name="Bomb"))
+
+
+def test_modify_group_header(app):
+    app.group_managment.modify_first_group(Group(header="Bomber"))
+
+
+def test_modify_group_footer(app):
     app.group_managment.modify_first_group(Group(footer="Bomberman"))
-    app.session.Logout()
