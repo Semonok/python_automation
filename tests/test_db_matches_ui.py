5134991753d9228adc3d08990f2183a434cf62c1
Semonok <semonok95@gmail.com>
2018-04-02 02:16:25 +0500

    create test for matching db and ui groups

---
+++ tests/test_db_matches_ui.py
@@ -0,0 +1,8 @@
+from model.group import Group
+
+def test_matches_db_and_ui(app, db):
+    groups_ui = app.group_managment.get_group_list()
+    def clean(group):
+        return Group(id=group.id,name=group.name.strip())
+    groups_db = map(clean, db.get_group_list())
+    assert sorted(groups_ui, key=Group.id_or_max) == sorted(groups_db, key=Group.id_or_max)
