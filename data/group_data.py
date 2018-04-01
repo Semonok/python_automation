4cdb1ff8913ee476c0de226b5d89b17f10951c71
Semonok <semonok95@gmail.com>
2018-04-02 02:14:29 +0500

    add coments

--- data/group_data.py
+++ data/group_data.py
@@ -5,7 +5,7 @@ import jsonpickle
 
 from model.group import Group
 
-
+# Генератор тестовых данных для групп
 def random_string(maxlen):
     symbols = string.digits + string.ascii_letters + string.punctuation + ' '
     return ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
@@ -17,8 +17,8 @@ testdata = [
     for header in ['', random_string(10)]
     for footer in ['', random_string(10)]]
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/groups.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/groups.json")  # указали путь для файла
 
-with open(file, "w") as f:
-    jsonpickle.set_encoder_options("json", indent=2)
-    f.write(jsonpickle.encode(testdata))
+with open(file, "w") as f:  # открыли файл на запись
+    jsonpickle.set_encoder_options("json", indent=2)  # настроили перенос строки и отступ с помощью 'indent'
+    f.write(jsonpickle.encode(testdata))  # закодировали в файл
